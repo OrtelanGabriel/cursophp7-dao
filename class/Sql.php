fab136e251504af65ed7b108af58dee656ca7bd2
OrtelanGabriel <ortelan.montanari@hotmail.com>
2022-08-10 12:32:58 -0300

    Metodos de lista

--- class/Sql.php
+++ class/Sql.php
@@ -43,7 +43,4 @@ class Sql extends PDO {
 
 }
 
-
-
-
  ?>
--- class/Usuario.php
+++ class/Usuario.php
@@ -58,6 +58,46 @@ class Usuario {
 			$this->setDessenha($row['dessenha']);
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 		}
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario;");
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY idusuario", array(
+			":SEARCH"=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0){
+
+			$row = $results[0];
+
+			$this->setIdUsuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos");
+			
+		}
 
 	}
 
@@ -69,7 +109,7 @@ class Usuario {
 			"dessenha"=>$this->getDessenha(),
 			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 		));
-		
+
 	}
 
 }
